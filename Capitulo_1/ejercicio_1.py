8ba140cada9d69080acda7e1b4b5f86cb5f96904
German Lopez Pineda <me1lopig@uco.es>
2021-02-01 22:23:20 +0100

    20210201

--- Capitulo_1/ejercicio_1.py
+++ Capitulo_1/ejercicio_1.py
@@ -1,5 +1,32 @@
 # pruebas con python
 
+# definicion de las funciones
+def operaciones(dato1,dato2):
+    #operaciones basicas
+    print("Cálculos realizados")
+    print("La suma de ",dato1," y de ",dato2," es ",dato1+dato2)
+    print("El producto de ",dato1," y de ",dato2," es ",dato1*dato2)
+    print("La diferencia de ",dato1," y de ",dato2," es ",dato1-dato2)
+    print("La división real de ",dato1," y de ",dato2," es ",dato1/dato2)
+    print("La division entera de ",dato1," y de ",dato2," es ",dato1//dato2)
+    print("El resto de la division entera de ",dato1," y de ",dato2," es ",dato1%dato2)
+    print("La potencia de  ",dato1," elevada a  ",dato2," es ",dato1**dato2)
+
+
+def compara(dato1,dato2):
+    # funcion de comparacion de los dos datos que se han introducido
+    if(dato1>dato2):
+        print("dato1=",dato1," es mayor que dato2=",dato2)
+    elif(dato1==dato2):
+        print("dato1=",dato1," es igual que dato2=",dato2)
+    else:
+        print("dato1=",dato1," es menor que dato2=",dato2)
+
+
+def determinaTipo(dato1,dato2):
+    # determinacion del tipo de dato
+    print("El dato ",dato1," es de tipo ",type(dato1))
+    print("El dato ",dato2," es de tipo ",type(dato2))
 
 print("Hola mundo")
 
@@ -8,22 +35,16 @@ print("Introduce dos números enteros ")
 dato1=int(input("Introduce el dato 1 "))
 dato2=int(input("Introduce el dato 2 "))
 
+# determinar el tipo de los datos
+determinaTipo(dato1,dato2)
 
-if(dato1>dato2):
-    print("dato1=",dato1," es mayor o igual que dato2=",dato2)
-elif(dato1==dato2):
-    print("dato1=",dato1," es igual que dato2=",dato2)
-else:
-    print("dato1=",dato1," es menor que dato2=",dato2)
+# Compara los dos datos
+compara(dato1,dato2)
 
-#operaciones basicas
-print("Cálculos realizados")
-print("La suma de ",dato1," y de ",dato2," es ",dato1+dato2)
-print("El producto de ",dato1," y de ",dato2," es ",dato1*dato2)
-print("La diferencia de ",dato1," y de ",dato2," es ",dato1-dato2)
-print("La división real de ",dato1," y de ",dato2," es ",dato1/dato2)
-print("La division entera de ",dato1," y de ",dato2," es ",dato1//dato2)
-print("El resto de la division entera de ",dato1," y de ",dato2," es ",dato1%dato2)
-print("La potencia de  ",dato1," elevada a  ",dato2," es ",dato1**dato2)
+
+# Llamado a la función
+operaciones(dato1,dato2)
+
+
 
 
