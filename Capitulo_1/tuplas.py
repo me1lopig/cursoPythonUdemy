c7283ce904a9a776d7fa0fed8d32fd78474154d6
German <glopez@ciccp.es>
2021-02-03 22:23:44 +0100

    modificacion

---
+++ Capitulo_1/tuplas.py
@@ -0,0 +1,12 @@
+# ejemplo del uso de tuplas
+tupla1=(12,34,12,32)
+print(tupla1)
+print(tupla1[:3])
+
+# para saber el indice en el que está un elemento, las tuplas admiten busquedas por indexao
+print(tupla1.index(34))
+
+# se pueden pasar listas a tuplas y al reves
+lista1=list(tupla1)
+print("Lista ",list)
+print("Tupla ",tupla1)
